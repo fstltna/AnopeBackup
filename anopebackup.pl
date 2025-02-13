0963297378de9ec953cb2e387a72ee257781200a
Marisa Giancarla <marisag@haikumail.online>
2025-02-13 12:45:53 -0800

    Now backs up the sql data too

--- anopebackup.pl
+++ anopebackup.pl
@@ -1,20 +1,112 @@
 #!/usr/bin/perl
 
 # Set these for your situation
-my $MTDIR = "/home/ircdaemon/services";
-my $BACKUPDIR = "/home/ircdaemon/backups";
+my $MTDIR = "/home/anopeuser/services";
+my $BACKUPDIR = "/home/anopeuser/backups";
 my $TARCMD = "/bin/tar czf";
-my $VERSION = "1.0.0";
+my $SQLDUMPCMD = "/usr/bin/mysqldump";
+my $VERSION = "1.7.2";
+my $OPTION_FILE = "/home/anopeuser/.anbackuprc";
+my $LATESTFILE = "$BACKUPDIR/anope.sql-1";
 my $DOSNAPSHOT = 0;
+my $MYSQLUSER = "";
+my $MYSQLPSWD = "";
+my $MYSQLDBNAME = "anope";
+my $FILEEDITOR = $ENV{EDITOR};
+
+if ($FILEEDITOR eq "")
+{
+	$FILEEDITOR = "/usr/bin/nano";
+}
+
+my $templatefile = <<'END_TEMPLATE';
+# Put mysql user here
+anope
+# Put mysql password here
+change-password
+# Put database name here
+anope
+END_TEMPLATE
+
 
 # Get if they said a option
 my $CMDOPTION = shift;
 
+sub ReadPrefs
+{
+	my $LineCount = 0;
+	if (! -f $OPTION_FILE)
+	{
+		open my $fh, '>', "$OPTION_FILE";
+		print ($fh $templatefile);
+		close($fh);
+		system("$FILEEDITOR $OPTION_FILE");
+	}
+
+	open(my $fh, '<:encoding(UTF-8)', $OPTION_FILE)
+		or die "Could not open file '$OPTION_FILE' $!";
+
+	while (my $row = <$fh>)
+	{
+		chomp $row;
+		if (substr($row, 0, 1) eq "#")
+		{
+			# Skip comment lines
+			next;
+		}
+
+		if ($LineCount == 0)
+		{
+			$MYSQLUSER = $row;
+		}
+		elsif ($LineCount == 1)
+		{
+			$MYSQLPSWD = $row;
+		}
+		elsif ($LineCount == 2)
+		{
+			$MYSQLDBNAME = $row;
+		}
+		$LineCount += 1;
+	}
+	close($fh);
+	if ($MYSQLUSER eq "")
+	{
+		print "Database username is empty - check the config file with \"anopebackup.pl -prefs\"\n";
+		exit;
+	}
+	if ($MYSQLPSWD eq "")
+	{
+		print "Database password is empty - check the config file with \"anopebackup.pl -prefs\"\n";
+		exit;
+	}
+	if ($MYSQLDBNAME eq "")
+	{
+		print "Database name is empty - check the config file with \"anopebackup.pl -prefs\"\n";
+		exit;
+	}
+	# print "User = $MYSQLUSER, PSWD = $MYSQLPSWD\n";
+}
+
+sub DumpMysql
+{
+	my $DUMPFILE = $_[0];
+
+	print "Backing up MYSQL data: ";
+	if (-f "$DUMPFILE")
+	{
+		unlink("$DUMPFILE");
+	}
+	# print "User = $MYSQLUSER, PSWD = $MYSQLPSWD\n";
+	system("$SQLDUMPCMD --user=$MYSQLUSER --password=$MYSQLPSWD --result-file=$DUMPFILE $MYSQLDBNAME");
+	print "\n";
+}
+
 if (defined $CMDOPTION)
 {
-	if ($CMDOPTION ne "-snapshot")
+	if (($CMDOPTION ne "-snapshot") && ($CMDOPTION ne "-prefs"))
 	{
-		print "Unknown command line option: '$CMDOPTION'\nOnly allowed options are '-snapshot'\n";
+		print "Unknown command line option: '$CMDOPTION'\nOnly allowed options are '-snapshot' and '-prefs'\n";
 		exit 0;
 	}
 }
@@ -27,7 +119,14 @@ sub SnapShotFunc
 		unlink("$BACKUPDIR/snapshot.tgz");
 	}
 	system("$TARCMD $BACKUPDIR/snapshot.tgz $MTDIR > /dev/null 2>\&1");
-	print "\nBackup Completed.\n";
+	print "\nBackup Completed.\nBacking up MYSQL data: ";
+	if (-f "$BACKUPDIR/snapshot.sql")
+	{
+		unlink("$BACKUPDIR/snapshot.sql");
+	}
+	# print "User = $MYSQLUSER, PSWD = $MYSQLPSWD\n";
+	DumpMysql("$BACKUPDIR/snapshot.sql");
+	print "\n";
 }
 
 #-------------------
@@ -45,6 +144,22 @@ if ($DOSNAPSHOT == -1)
 	print "Running Manual Snapshot\n";
 }
 print "==============================\n";
+
+if ((defined $CMDOPTION) && ($CMDOPTION eq "-prefs"))
+{
+	# Edit the prefs file
+	print "Editing the prefs file\n";
+	if (! -f $OPTION_FILE)
+	{
+		open my $fh, '>', "$OPTION_FILE";
+		print ($fh $templatefile);
+		close($fh);
+	}
+	system("$FILEEDITOR $OPTION_FILE");
+	exit 0;
+}
+
+ReadPrefs();
 
 if (! -d $BACKUPDIR)
 {
@@ -77,6 +192,23 @@ while ($FileRevision > 0)
 
 print "Done\nCreating New Backup: ";
 system("$TARCMD $BACKUPDIR/anopebackup-1.tgz $MTDIR");
-print "Done\n";
+print "Done\nMoving Existing MySQL data: ";
+if (-f "$BACKUPDIR/anope.sql-5")
+{
+	unlink("$BACKUPDIR/anope.sql-5") or warn "Could not unlink $BACKUPDIR/anope.sql-5: $!";
+}
 
+$FileRevision = 4;
+while ($FileRevision > 0)
+{
+	if (-f "$BACKUPDIR/anope.sql-$FileRevision")
+	{
+		my $NewVersion = $FileRevision + 1;
+		rename("$BACKUPDIR/anope.sql-$FileRevision", "$BACKUPDIR/anope.sql-$NewVersion");
+	}
+	$FileRevision -= 1;
+}
+
+DumpMysql($LATESTFILE);
+print("Done!\n");
 exit 0;
